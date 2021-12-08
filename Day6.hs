77a2aeabaa1a4cc72c2019384d3bc0541ca33d83
Jacob Peyron <jacob.peyron@evolvetechnology.se>
2021-12-08 08:48:59 +0100

    day6 complete

--- Day6.hs
+++ Day6.hs
@@ -1,34 +1,59 @@
 module Day6 where
 import Common
 
+-- each value corresponds to the number of fish of that index
+type Vec = [Int]
+
+-- convert occurrences of valid numbers to occurrence vector
+-- example input -> output pairs:
+--      [0]          -> [1, 0, 0, 0, 0, 0, 0, 0, 0]
+--      [8]          -> [0, 0, 0, 0, 0, 0, 0, 0, 1]
+--      [1, 2, 3, 3] -> [0, 1, 1, 2, 0, 0, 0, 0, 0]
+newvec :: [Int] -> Vec
+newvec xs = foldl sum' h t  where
+    (h,t) = (head units, tail units)
+    units = map unitvec xs
+    
+-- unit vector
+-- example i -> o
+--      0 -> [1, 0, 0, 0, 0, 0, 0, 0, 0]
+--      2 -> [0, 0, 1, 0, 0, 0, 0, 0, 0]
+--      8 -> [0, 0, 0, 0, 0, 0, 0, 0, 1]
+unitvec :: Int -> Vec
+unitvec n = modifyN n 1 zero where
+    zero = replicate 9 0
+
+-- vector sum
+sum' :: Vec -> Vec -> Vec
+sum' a b = map esum (zip a b) where
+    esum = \(k,l) -> k+l
+
+-- parse
 parse :: String -> [Int]
 parse xs = (map atoi) . wordsWhen (==',') $ xs
 
--- make problem go from 0 -> 6 instead of 6 -> 0
+-- step simulation once
+step :: Vec -> Vec
+step xs =  sum' zvec shifted where 
+    zvec = map (*zunit) zaction
+    shifted = tail xs ++ [0]
 
-fmod = flip mod
-fmod7 = fmod 7
-invert n = fmod7 (6-n) 
+    zunit = head xs
+    zaction = sum' (unitvec 6) (unitvec 8)
 
--- naive (first) approach
-step' :: [Int] -> [Int]
-step' xs = stepped ++ replicate zeroes 8 where 
-    stepped = map stepper xs
-    zeroes = count (==0) xs
-    stepper = \x -> if x == 0 then 6 else x-1
+-- step simulation n times
+multistep :: [Int] -> Int -> [Int]
+multistep xs 0 = xs
+multistep xs n = multistep (step xs) (n-1)
 
-multistep' :: Int -> [Int] -> [Int]
-multistep' 0 xs = xs
-multistep' n xs = multistep' (n-1) (step' xs)
-
-slv :: Int -> [Int] -> Int
-slv n = length . (multistep' n) 
+slv :: [Int] -> Int -> Int
+slv xs = sum . (multistep (newvec xs)) 
 
 solve :: String -> Int
-solve xs = slv 80 $ parse xs
+solve xs = slv (parse xs) 80
 
 solve' :: String -> Int
-solve' xs = slv 256 $ parse xs
+solve' xs = slv (parse xs) 256
 
 main :: IO ()
 main = do
