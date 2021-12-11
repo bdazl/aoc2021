1abefb322a0fbcecf784ccf1a67cdee7386edd84
Jacob Peyron <jacob.peyron@evolvetechnology.se>
2021-12-11 15:21:42 +0100

    transpose is in Data.List

--- Common.hs
+++ Common.hs
@@ -46,6 +46,7 @@ wordsWhen p s = case dropWhile p s of
                     s' -> w : wordsWhen p s''
                         where (w, s'') = break p s'
 
+-- use import Data.List, it's there...
 transpose :: [[a]] -> [[a]]
 transpose ([]:_) = []
 transpose xs = (map head xs) : transpose (map tail xs)
